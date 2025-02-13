0f0f50c0e03f5e389da2ad5ee3a8395bf2e02af7
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2025-02-13 10:06:49 +0530

    fix: application route

--- src/app/api/applications/[id]/route.ts
+++ src/app/api/applications/[id]/route.ts
@@ -1,40 +1,46 @@
-import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
+import { z } from "zod";
 
-export async function GET(
-  request: NextRequest,
-  context: { params: { id: string } }
-) {
-  const jobId = context.params.id;
-  if (!jobId) {
-    return NextResponse.json({ error: "Job Id is required" }, { status: 400 });
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(req: Request, { params }: Props) {
+  const idSchema = z.string().min(1);
+  const idValidation = idSchema.safeParse(params.id);
+
+  if (!idValidation.success) {
+    return NextResponse.json({ error: "Invalid job ID" }, { status: 400 });
   }
+
+  const jobId = params.id;
 
   try {
     const getApplications = await prisma.application.findMany({
       where: { jobId },
-      orderBy: { submittedAt: "desc" },
+      orderBy: {
+        submittedAt: "desc",
+      },
     });
 
-    if (getApplications.length === 0) {
+    if (!getApplications.length) {
       return NextResponse.json(
         { error: "No applications found" },
         { status: 404 }
       );
     }
 
-    return NextResponse.json(
-      { applications: getApplications },
-      { status: 200 }
-    );
+    return NextResponse.json({ applications: getApplications });
   } catch (error) {
     console.error("Error in getting applications:", error);
     return NextResponse.json(
       {
         error:
           process.env.NODE_ENV === "development"
-            ? (error as Error).message
+            ? String(error)
             : "An error occurred while fetching the applications.",
       },
       { status: 500 }
