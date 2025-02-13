f4198e199518003ae3839bc5bb48994e975c9b64
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2025-02-13 10:12:03 +0530

    feat: add types in application route

--- src/app/api/applications/[id]/route.ts
+++ src/app/api/applications/[id]/route.ts
@@ -2,21 +2,15 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { z } from "zod";
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-export async function GET(req: Request, { params }: Props) {
+export async function GET(req: Request, context: { params: { id: string } }) {
   const idSchema = z.string().min(1);
-  const idValidation = idSchema.safeParse(params.id);
+  const idValidation = idSchema.safeParse(context.params.id);
 
   if (!idValidation.success) {
     return NextResponse.json({ error: "Invalid job ID" }, { status: 400 });
   }
 
-  const jobId = params.id;
+  const jobId = context.params.id;
 
   try {
     const getApplications = await prisma.application.findMany({
